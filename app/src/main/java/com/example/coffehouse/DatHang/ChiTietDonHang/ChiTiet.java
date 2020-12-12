cfc6af75a1e86cf26801ef7b2e3a7151632a8a20
nhocvukhoa <nvanhkhoa148@gmail.com>
2020-12-12 15:04:27 +0700

    first commit

---
+++ app/src/main/java/com/example/coffehouse/DatHang/ChiTietDonHang/ChiTiet.java
@@ -0,0 +1,14 @@
+package com.example.coffehouse.DatHang.ChiTietDonHang;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.coffehouse.R;
+public class ChiTiet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chi_tiet);
+    }
+}
